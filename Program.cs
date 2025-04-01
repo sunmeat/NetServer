cda45c026abbdc25d9c5b57d679658e3f967bc0c
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-04-01 11:28:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        const int port = 5000;
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Сервер запущен на порту {port}");
+
+        while (true)
+        {
+            using var client = listener.AcceptTcpClient();
+            using var stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            if (int.TryParse(receivedData, out int number))
+            {
+                int result = number + 1;
+                byte[] response = Encoding.UTF8.GetBytes(result.ToString());
+                stream.Write(response, 0, response.Length);
+                Console.WriteLine($"Получено: {number}, отправлено: {result}");
+            }
+        }
+    }
+}
